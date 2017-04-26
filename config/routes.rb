743b88414a7064c37b08c15e6d8daffde64a3b87
Antonio Ricardo <antonio.eschola@gmail.com>
2017-04-25 22:56:18 -0400

    - Create model user - Create file test - Adding devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'ApiVersionConstraint'
 
 Rails.application.routes.draw do
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json}, path: '/' do
       namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
@@ -11,4 +13,5 @@ Rails.application.routes.draw do
       end
 =end
   end
+
 end
